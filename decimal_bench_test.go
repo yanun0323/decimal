09fc5662a4c1e2bbba5b1f92bbe0ec7beeaf5813
yanun0323 <a1256212562@gmail.com>
2023-11-23 20:13:26 +0800

    udpate readme and test

--- decimal_bench_test.go
+++ decimal_bench_test.go
@@ -13,241 +13,261 @@ const (
 	_operatorAddition = "789.00456888"
 )
 
-// func BenchmarkNew(b *testing.B) {
-// 	for i := 0; i < b.N; i++ {
-// 		b, _ := New(_operatorBase)
-// 		_ = b
-// 	}
-// }
-
-// Add
-
-func BenchmarkAddShopSpringDecimal(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		b, _ := decimal.NewFromString(_operatorBase)
-		a, _ := decimal.NewFromString(_operatorAddition)
-		result := b.Add(a).Add(b)
-		ss := result.String()
-		_ = ss
-
-		b.Mul(a)
-
-	}
-}
-
-func BenchmarkAddDecimal(b *testing.B) {
+func BenchmarkNew(b *testing.B) {
+	b.SkipNow()
 	for i := 0; i < b.N; i++ {
 		b, _ := New(_operatorBase)
-		a, _ := New(_operatorAddition)
-		result := b.Add(a).Add(a)
-		_ = result
+		_ = b
 	}
 }
 
-// Sub
+func Run(b *testing.B, shop, dec func(b *testing.B)) {
+	b.Run("ShopSpringDecimal", func(b *testing.B) {
+		shop(b)
+	})
 
-func BenchmarkSubShopSpringDecimal(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		b, _ := decimal.NewFromString(_operatorBase)
-		a, _ := decimal.NewFromString(_operatorAddition)
-		result := b.Sub(a).Sub(b)
-		ss := result.String()
-		_ = ss
-
-		b.Mul(a)
-
-	}
+	b.Run("Decimal", func(b *testing.B) {
+		dec(b)
+	})
 }
 
-func BenchmarkSubDecimal(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		b, _ := New(_operatorBase)
-		a, _ := New(_operatorAddition)
-		result := b.Sub(a).Sub(a)
-		_ = result
-	}
+func BenchmarkAdd(b *testing.B) {
+	Run(b,
+		func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				b, _ := decimal.NewFromString(_operatorBase)
+				a, _ := decimal.NewFromString(_operatorAddition)
+				result := b.Add(a).Add(b)
+				ss := result.String()
+				_ = ss
+			}
+		},
+		func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				b, _ := New(_operatorBase)
+				a, _ := New(_operatorAddition)
+				result := b.Add(a).Add(a)
+				_ = result
+			}
+		},
+	)
 }
 
-// Mul
+func BenchmarkSub(b *testing.B) {
+	Run(b,
+		func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				b, _ := decimal.NewFromString(_operatorBase)
+				a, _ := decimal.NewFromString(_operatorAddition)
+				result := b.Sub(a).Sub(b)
+				ss := result.String()
+				_ = ss
 
-func BenchmarkMulShopSpringDecimal(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		d1, _ := decimal.NewFromString(_operatorBase)
-		d2, _ := decimal.NewFromString(_operatorAddition)
-		_ = d1.Mul(d2)
-	}
+				b.Mul(a)
+
+			}
+		},
+		func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				b, _ := New(_operatorBase)
+				a, _ := New(_operatorAddition)
+				result := b.Sub(a).Sub(a)
+				_ = result
+			}
+		},
+	)
 }
 
-func BenchmarkMulDecimal(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		d1, _ := New(_operatorBase)
-		d2, _ := New(_operatorAddition)
-		_ = d1.Mul(d2)
-	}
+func BenchmarkMul(b *testing.B) {
+	Run(b,
+		func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				d1, _ := decimal.NewFromString(_operatorBase)
+				d2, _ := decimal.NewFromString(_operatorAddition)
+				_ = d1.Mul(d2)
+			}
+		},
+		func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				d1, _ := New(_operatorBase)
+				d2, _ := New(_operatorAddition)
+				_ = d1.Mul(d2)
+			}
+		},
+	)
 }
 
-// Shifting
-
-func BenchmarkShiftShopSpringDecimal(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		d, _ := decimal.NewFromString(_operatorBase)
-		s := d.Shift(8).String()
-		_ = s
-		d2, _ := decimal.NewFromString(_operatorBase)
-		s2 := d2.Shift(-8).String()
-		_ = s2
-	}
+func BenchmarkShift(b *testing.B) {
+	Run(b,
+		func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				d, _ := decimal.NewFromString(_operatorBase)
+				s := d.Shift(8).String()
+				_ = s
+				d2, _ := decimal.NewFromString(_operatorBase)
+				s2 := d2.Shift(-8).String()
+				_ = s2
+			}
+		},
+		func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				d, _ := New(_operatorBase)
+				s := d.Shift(8)
+				_ = s
+				d2, _ := New(_operatorBase)
+				s2 := d2.Shift(-8)
+				_ = s2
+			}
+		},
+	)
 }
 
-func BenchmarkShiftDecimal(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		d, _ := New(_operatorBase)
-		s := d.Shift(8)
-		_ = s
-		d2, _ := New(_operatorBase)
-		s2 := d2.Shift(-8)
-		_ = s2
-	}
+func BenchmarkNeg(b *testing.B) {
+	Run(b,
+		func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				d1, _ := decimal.NewFromString(_operatorBase)
+				_ = d1.Neg()
+			}
+		},
+		func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				d1, _ := New(_operatorBase)
+				_ = d1.Neg()
+			}
+		},
+	)
 }
 
-// Neg
-
-func BenchmarkNegShopSpringDecimal(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		d1, _ := decimal.NewFromString(_operatorBase)
-		_ = d1.Neg()
-	}
+func BenchmarkTruncate(b *testing.B) {
+	Run(b,
+		func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				d, _ := decimal.NewFromString(_operatorBase)
+				s := d.Truncate(8).String()
+				_ = s
+				d2, _ := decimal.NewFromString(_operatorBase)
+				s2 := d2.Truncate(-8).String()
+				_ = s2
+			}
+		},
+		func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				d, _ := New(_operatorBase)
+				s := d.Truncate(8)
+				_ = s
+				d2, _ := New(_operatorBase)
+				s2 := d2.Truncate(-8)
+				_ = s2
+			}
+		},
+	)
 }
 
-func BenchmarkNegDecimal(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		d1, _ := New(_operatorBase)
-		_ = d1.Neg()
-	}
+func BenchmarkIsZero(b *testing.B) {
+	Run(b,
+		func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				d1, _ := decimal.NewFromString(_operatorBase)
+				_ = d1.IsZero()
+			}
+		},
+		func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				d1, _ := New(_operatorBase)
+				_ = d1.IsZero()
+			}
+		},
+	)
 }
 
-// Truncating
-
-func BenchmarkTruncateShopSpringDecimal(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		d, _ := decimal.NewFromString(_operatorBase)
-		s := d.Truncate(8).String()
-		_ = s
-		d2, _ := decimal.NewFromString(_operatorBase)
-		s2 := d2.Truncate(-8).String()
-		_ = s2
-	}
+func BenchmarkIsPositive(b *testing.B) {
+	Run(b,
+		func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				d1, _ := decimal.NewFromString(_operatorBase)
+				_ = d1.IsPositive()
+			}
+		},
+		func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				d1, _ := New(_operatorBase)
+				_ = d1.IsPositive()
+			}
+		},
+	)
 }
 
-func BenchmarkTruncateDecimal(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		d, _ := New(_operatorBase)
-		s := d.Truncate(8)
-		_ = s
-		d2, _ := New(_operatorBase)
-		s2 := d2.Truncate(-8)
-		_ = s2
-	}
+func BenchmarkIsNegative(b *testing.B) {
+	Run(b,
+		func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				d1, _ := decimal.NewFromString(_operatorBase)
+				_ = d1.IsNegative()
+			}
+		},
+		func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				d1, _ := New(_operatorBase)
+				_ = d1.IsNegative()
+			}
+		},
+	)
 }
 
-// IsZero
-
-func BenchmarkIsZeroShopSpringDecimal(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		d1, _ := decimal.NewFromString(_operatorBase)
-		_ = d1.IsZero()
-	}
+func BenchmarkEqual(b *testing.B) {
+	Run(b,
+		func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				d1, _ := decimal.NewFromString(_operatorBase)
+				d2, _ := decimal.NewFromString(_operatorAddition)
+				_ = d1.Equal(d2)
+			}
+		},
+		func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				d1, _ := New(_operatorBase)
+				d2, _ := New(_operatorAddition)
+				_ = d1.Equal(d2)
+			}
+		},
+	)
 }
 
-func BenchmarkIsZeroDecimal(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		d1, _ := New(_operatorBase)
-		_ = d1.IsZero()
-	}
+func BenchmarkGreater(b *testing.B) {
+	Run(b,
+		func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				d1, _ := decimal.NewFromString(_operatorBase)
+				d2, _ := decimal.NewFromString(_operatorAddition)
+				_ = d1.GreaterThan(d2)
+			}
+		},
+		func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				d1, _ := New(_operatorBase)
+				d2, _ := New(_operatorAddition)
+				_ = d1.Greater(d2)
+			}
+		},
+	)
 }
 
-// IsPositive
-
-func BenchmarkIsPositiveShopSpringDecimal(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		d1, _ := decimal.NewFromString(_operatorBase)
-		_ = d1.IsPositive()
-	}
-}
-
-func BenchmarkIsPositiveDecimal(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		d1, _ := New(_operatorBase)
-		_ = d1.IsPositive()
-	}
-}
-
-// IsNegative
-
-func BenchmarkIsNegativeShopSpringDecimal(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		d1, _ := decimal.NewFromString(_operatorBase)
-		_ = d1.IsNegative()
-	}
-}
-
-func BenchmarkIsNegativeDecimal(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		d1, _ := New(_operatorBase)
-		_ = d1.IsNegative()
-	}
-}
-
-// Equal
-
-func BenchmarkEqualShopSpringDecimal(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		d1, _ := decimal.NewFromString(_operatorBase)
-		d2, _ := decimal.NewFromString(_operatorAddition)
-		_ = d1.Equal(d2)
-	}
-}
-
-func BenchmarkEqualDecimal(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		d1, _ := New(_operatorBase)
-		d2, _ := New(_operatorAddition)
-		_ = d1.Equal(d2)
-	}
-}
-
-// Greater
-
-func BenchmarkGreaterShopSpringDecimal(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		d1, _ := decimal.NewFromString(_operatorBase)
-		d2, _ := decimal.NewFromString(_operatorAddition)
-		_ = d1.GreaterThan(d2)
-	}
-}
-
-func BenchmarkGreaterDecimal(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		d1, _ := New(_operatorBase)
-		d2, _ := New(_operatorAddition)
-		_ = d1.Greater(d2)
-	}
-}
-
-// Less
-
-func BenchmarkLessShopSpringDecimal(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		d1, _ := decimal.NewFromString(_operatorBase)
-		d2, _ := decimal.NewFromString(_operatorAddition)
-		_ = d1.LessThan(d2)
-	}
-}
-
-func BenchmarkLessDecimal(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		d1, _ := New(_operatorBase)
-		d2, _ := New(_operatorAddition)
-		_ = d1.Less(d2)
-	}
+func BenchmarkLess(b *testing.B) {
+	Run(b,
+		func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				d1, _ := decimal.NewFromString(_operatorBase)
+				d2, _ := decimal.NewFromString(_operatorAddition)
+				_ = d1.LessThan(d2)
+			}
+		},
+		func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				d1, _ := New(_operatorBase)
+				d2, _ := New(_operatorAddition)
+				_ = d1.Less(d2)
+			}
+		},
+	)
 }
