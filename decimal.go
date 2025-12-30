bf24f90b568e1f2f6d832edde18174e1a02de004
yanun0323 <yanun0323@gmail.com>
2025-12-30 16:04:06 +0800

    fix: define decimal

---
+++ decimal.go
@@ -0,0 +1,50 @@
+package decimal
+
+// Don't use this. just for definition.
+type Decimal interface {
+	// conversions
+	Int64() (intPart, decimalPart int64)
+	Float64() float64
+	String() string
+	StringFixed(int) string
+
+	// checking
+	IsZero() bool
+	IsPositive() bool
+	IsNegative() bool
+	Sign() int // 0:zero 1:pos 2:neg
+
+	// modification
+	Neg() Decimal
+	Inv() Decimal
+	Abs() Decimal
+	Truncate(int) Decimal
+	Shift(int) Decimal
+	Round(int) Decimal
+	RoundAwayFromZero(int) Decimal
+	RoundTowardToZero(int) Decimal
+	Ceil(int) Decimal
+	Floor(int) Decimal
+
+	// comparison
+	Equal(Decimal) bool
+	GreaterThan(Decimal) bool
+	LessThan(Decimal) bool
+	GreaterOrEqual(Decimal) bool
+	LessOrEqual(Decimal) bool
+
+	// arithmetic operations
+	Add(Decimal) Decimal
+	Sub(Decimal) Decimal
+	Mul(Decimal) Decimal
+	Div(Decimal) Decimal
+	Mod(Decimal) Decimal
+
+	// transcendental operations
+	Pow(Decimal) Decimal
+	Sqrt() Decimal
+	Exp() Decimal
+	Log() Decimal
+	Log2() Decimal
+	Log10() Decimal
+}
